d4604a7f4dd39a1040a07d16be77582076d02a4d
Eric <ericlg.link@gmail.com>
2022-10-11 18:34:41 -0700

    Export new Gallery function to place in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react';
 
 function App() {
+  let [data, setData] = useState({})
+  let [artId, setArtId] = useState(12720)
 
   useEffect(() => {
     document.title = 'Welcome to ArtWorld'
-  })
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+  }, [artId])
 }
 
 export default App;
